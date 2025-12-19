628dcd0022c2afb5987406920ed8b8b67dfaf569
ahtius13 <davidinformatico18@gmail.com>
2025-12-19 02:49:17 +0100

    Subida completa del proyecto PalParne

---
+++ saldo.py
@@ -0,0 +1,12 @@
+# Saldo.py
+# Este archivo te muestra el salctual del usuario actualizado.
+
+# 1. FUNCIÓN PARA CONSULTAR SALDO_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+def consultar_saldo(saldo):
+
+    # Esta función recibe el saldo actual como parámetro
+
+    # Mostramos el saldo por pantalla usando print
+
+    print("Tu saldo actual es:", saldo, "euros")
